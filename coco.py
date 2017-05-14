1cc4f8bdda0f879c261fcc844d43cd33e3475268
xXcocol21190Xx <cocol21190@gmail.com>
2017-05-14 22:40:26 +0200

    Add files via upload

--- coco.py
+++ coco.py
@@ -16,6 +16,18 @@ def set_filename():
     pygame.mixer.music.play()
 
 FILETYPES = [ ("Fichiers musicaux", "*.mp3") ]
+def precedent():
+    a = 0
+    a = pygame.mixer.music.get_pos()
+    if a < 3:
+        pygame.mixer.music.load(exchemin)
+        pygame.mixer.music.play()
+    else:
+        pygame.mixer.music.rewind()
+    print(a)
+def stop():
+    pygame.mixer.music.rewind()
+    pygame.mixer.music.pause()
 
 pygame.mixer.init()
 
@@ -33,10 +45,10 @@ boutonPause.pack()
 boutonPlay = Button(fenetre, text="Lecture", command = mixer.music.unpause, fg="black", background = "#FFFFFF", height = "3", width = "6")
 boutonPlay.pack()
 
-boutonDebut = Button(fenetre, text="Début", command = mixer.music.play, fg="DARKBLUE", background = "#C8C8C8", height = "1", width = "5")
-boutonDebut.pack()
+'''boutonPrecedent = Button(fenetre, text="Précédent", command = precedent(), fg="DARKBLUE", background = "#C8C8C8", height = "1", width = "5")
+boutonPrecedent.pack()'''
 
-boutonStop = Button(fenetre, text="Stop", command = mixer.music.stop, fg="red", background = "#C8C8C8", height = "1", width = "4")
+boutonStop = Button(fenetre, text="Stop", command = stop(), fg="red", background = "#C8C8C8", height = "1", width = "4")
 boutonStop.pack()
 
 fenetre.mainloop()
