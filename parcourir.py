77c2584f7a372876b1b26462d16cf3ffa2c802af
xXcocol21190Xx <cocol21190@gmail.com>
2017-05-15 20:57:23 +0200

    Add files via upload

---
+++ parcourir.py
@@ -0,0 +1,12 @@
+﻿def set_filename():
+
+    chemin = StringVar(fenetre)
+    chemin = askopenfilename(filetypes=FILETYPES)
+
+    pygame.mixer.music.load(chemin)
+    pygame.mixer.music.play()
+
+FILETYPES = [ ("Fichiers musicaux", "*.mp3") ]
+
+boutonParcourir = Button(fenetre, text='Parcourir', command=set_filename)
+boutonParcourir.place(x=280,y=140)
